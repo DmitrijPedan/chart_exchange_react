4353c8309755ac9880e674e0bfff070d9f7931f9
DmitrijPedan <dmitrijpedan84@gmail.com>
2020-03-16 15:08:32 +0200

    connect stocksArea

--- src/components/stocksArea.js
+++ src/components/stocksArea.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import {connect} from 'react-redux';
 
-const StocksArea = () => {
-   
+
+const StocksArea = ({ws, data}) => {
+    
+    const wss = new WebSocket('ws://thawing-depths-65790.herokuapp.com/stocks');
+    // wss.onopen = e => console.log('connected');
+    // wss.onmessage = e => console.log(e);
+
+    console.log('ws from store: ', ws);
+    console.log('new ws: ', wss);
+    
+    
     return (
         <div className = "toolbar-element stockElement hidden">
             <div>StockArea</div>
@@ -9,4 +19,9 @@ const StocksArea = () => {
     )
 }
 
-export default StocksArea;
+const mapStateToProps = (state) => ({
+    ws: state.wsConnection.ws,
+    data: state.wsConnection.data,
+})
+
+export default connect(mapStateToProps)(StocksArea);
--- src/store/reducers/setWSConnectionReducer.js
+++ src/store/reducers/setWSConnectionReducer.js
@@ -1,15 +1,27 @@
-import {SET_WS_CONNECTION} from '../constants';
+import {SET_WS_CONNECTION, SET_STOCKS_DATA} from '../constants';
 
-const initState = null;
+const initState = {
+    ws: null,
+    data: null,
+};
 
 const reducer = (state = initState, {type, payload}) => {
     switch (type) {
         case SET_WS_CONNECTION:
+            return {
+                ...state,
+                ws: payload,
+            };
+        case SET_STOCKS_DATA:
             console.log('payload', payload);
-            return {...payload};
+            return {
+                ...state,
+                data: payload,
+            };
         default:
             return {...state};
-    }
+    }   
 }
 
 export default reducer;
+
