1a6fb3d3f8f4f854b439a9f840b338f609c11235
DmitrijPedan <dmitrijpedan84@gmail.com>
2020-04-03 14:04:50 +0300

    add future dates

--- src/services/getChartData.js
+++ src/services/getChartData.js
@@ -2,13 +2,16 @@ import moment from 'moment';
 import {borderColors} from '../config/chartConfig';
 
 export const getChartData = (period, dates, currencies) => {
+    console.log(period);
+    
     return {
         labels: dates.map(el => moment(el).format('DD.MM.YYYY')),
         datasets: currencies.map(code => {
             let rates = period.map(el => el.find(elem => elem.cc === code))
+            console.log(rates)
             return {
                 label: code,
-                data: rates.map(el => el.rate),
+                data: rates.map(el => el ? el.rate : null),
                 backgroundColor: ['rgba(255, 255, 255, 0)'],
                 borderColor: [borderColors[Math.floor(Math.random() * borderColors.length)]],
                 borderWidth: 1,
